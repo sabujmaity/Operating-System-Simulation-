16ede6874410ef061d07278db5313bb23b852eda
SABUJ MAITY <34521697+SabujPro@users.noreply.github.com>
2020-03-29 22:06:08 +0530

    Add files via upload

---
+++ Final File.c
@@ -0,0 +1,65 @@
+#include <stdio.h> 
+int main() 
+{ 
+	int n, m, i, j, k; 
+    n=5; 
+    m=4;
+    int alloc[5][4] ={{0,0,1,2},
+                      {1,0,0,0},
+                      {1,3,5,4},
+                      {0,6,3,2},
+                      {0,0,1,4}};
+  
+    int max[5][4]={{0,0,1,2},
+                   {1,7,5,0},
+                   {2,3,5,6},
+                   {0,6,5,2},
+                   {0,6,5,6}};
+  
+    int avail[4]={1,5,2,0};  
+    int f[n],ans[n],ind=0; 
+    for (k=0;k<n;k++) 
+	{ 
+        f[k] = 0; 
+    } 
+    int need[n][m]; 
+    for (i=0;i<n;i++) 
+	{ 
+        for (j=0;j<m;j++) 
+            need[i][j] = max[i][j] - alloc[i][j]; 
+    } 
+    int y=0; 
+    for (k=0;k<5;k++) 
+	{ 
+        for (i=0;i<n;i++) 
+		{ 
+            if (f[i]==0) 
+			{  
+                int flag = 0; 
+                for (j=0;j<m;j++) 
+				{ 
+                    if (need[i][j] > avail[j])
+					{ 
+                        flag = 1; 
+                        break; 
+                    } 
+                } 
+  
+                if (flag==0) 
+				{ 
+                    ans[ind++] = i; 
+                    for (y = 0; y < m; y++) 
+                        avail[y] += alloc[i][y]; 
+                    f[i] = 1; 
+                } 
+            } 
+        } 
+    } 
+  
+    printf("Following is the SAFE Sequence\n"); 
+    for (i = 0; i < n - 1; i++) 
+        printf(" P%d ->", ans[i]); 
+    printf(" P%d", ans[n - 1]); 
+  
+    return (0);  
+} 
